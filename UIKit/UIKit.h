049704ec6e11fa342380cfda0ac35812aaf104f2
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2013-07-18 17:48:50 -0700

    Initial Commit

---
+++ UIKit/UIKit.h
@@ -0,0 +1,11 @@
+//
+//  UIKit.h
+//  UIKit
+//
+//  Created by Kevin MacWhinnie on 7/18/13.
+//  Copyright (c) 2013 Roundabout Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
---
+++ UIKitTests/UIKitTests.m
@@ -0,0 +1,36 @@
+//
+//  UIKitTests.m
+//  UIKitTests
+//
+//  Created by Kevin MacWhinnie on 7/18/13.
+//  Copyright (c) 2013 Roundabout Software, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIKitTests : XCTestCase
+
+@end
+
+@implementation UIKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
